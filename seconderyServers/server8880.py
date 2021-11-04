b1e584c72cab95e84464c307d91505e5e1b188c0
Shelly Shuster <shelly.shuster2004@gmail.com>
2021-11-04 20:28:14 +0200

    Fix secondaty servers'

--- seconderyServers/server8880.py
+++ seconderyServers/server8880.py
@@ -3,13 +3,13 @@ import socket
 import threading
 
 HOST = '127.0.0.1'  # Standard loopback interface address
-PORT = 8880         # Port to listen on
+PORT = 8881         # Port to listen on
 
 def sentToNextClient(ip, port, msg):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip, int(port)))
 
-    client.send(msg)
+    client.send(msg.encode())
 
     client.close()
 
@@ -25,7 +25,8 @@ def thread(conn):
         next_ip = list[-2]
         next_port = list[-1]
         print(next_ip + " and " + next_port)
-        conn.send(list[0].encode())
+        print(list[0])
+        sentToNextClient(next_ip, next_port, list[0])
     conn.close()
 
 
