c7ed0855f34b1a654d8e8b9b28c6dc8c3e69318b
Shelly Shuster <shelly.shuster2004@gmail.com>
2021-11-06 16:23:55 +0200

    Export client thread to a different file

--- MainServer/ClientThread.py
+++ MainServer/ClientThread.py
@@ -1,22 +1,43 @@
 import socket, threading
+LOCALHOST = "127.0.0.1"
+FIRST_PORT = "9876"
+SECOND_PORT = "8081"
+LISTEN_PORT = 5678
+
+SPACER = "::::"
+
 class ClientThread(threading.Thread):
-    
-    def __init__(self,clientAddress,clientsocket):
+
+    def __init__(self, clientAddress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
-        print ("New connection added: ", clientAddress)
-        
+        self.clientAddress = clientAddress
+        print("New connection added: ", clientAddress)
+
+    # def createMessageRoute(self, messageContent):
+    #     msg = messageContent + SPACER + LOCALHOST +FIRST_PORT + SPACER + LOCALHOST + SECOND_PORT
+    #     return  msg
+
     def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        msg = 'hi::::127.0.0.1::::8880::::127.0.0.1::::8882'
-        #while True:
-            # data = self.csocket.recv(2048)
-            # msg = data.decode()
-            # if msg=='bye':
-            #   break
-            # print ("from client", msg)
-        print("check")
-        self.csocket.send(bytes(msg,'UTF-8'))
-        print("check after sending")
-        print ("Client at ", clientAddress , " disconnected...")
+        print("Connection from : ", self.clientAddress)
+        msg = ''
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            if msg=='bye':
+               break
+            print ("from client", msg)
+            self.csocket.send(bytes("main server received message "+ msg, 'UTF-8'))
+        #'hi::::127.0.0.1::::8881::::127.0.0.1::::8882'
+        #msg = self.createMessageRoute(messageContent=msg)
+            msg += SPACER + LOCALHOST + SPACER + FIRST_PORT + SPACER + LOCALHOST+ SPACER + SECOND_PORT
+            print("creates message + route = " + msg )
+            sendToFirstClient(LOCALHOST, 8881, msg)
+        print("Client at ", self.clientAddress, " disconnected...")
+
+
+def sendToFirstClient(ip, port, msg):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((ip, int(port)))
+    client.send(msg.encode())
+    client.close()
--- MainServer/pythonServer.py
+++ MainServer/pythonServer.py
@@ -1,4 +1,5 @@
 import socket, threading
+from ClientThread import  ClientThread
 
 LOCALHOST = "127.0.0.1"
 FIRST_PORT = "9876"
@@ -8,38 +9,34 @@ LISTEN_PORT = 5678
 SPACER = "::::"
 
 
-class ClientThread(threading.Thread):
-
-    def __init__(self, clientAddress, clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        self.clientAddress = clientAddress
-        print("New connection added: ", clientAddress)
-
-    # def createMessageRoute(self, messageContent):
-    #     msg = messageContent + SPACER + LOCALHOST +FIRST_PORT + SPACER + LOCALHOST + SECOND_PORT
-    #     return  msg
-
-    def run(self):
-        print("Connection from : ", self.clientAddress)
-        msg = ''
-        while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg=='bye':
-               break
-            print ("from client", msg)
-            self.csocket.send(bytes("main server received message "+ msg, 'UTF-8'))
-        #'hi::::127.0.0.1::::8881::::127.0.0.1::::8882'
-        #msg = self.createMessageRoute(messageContent=msg)
-            msg += SPACER + LOCALHOST + SPACER + FIRST_PORT + SPACER + LOCALHOST+ SPACER + SECOND_PORT
-            print("creates message + route = " + msg )
-            sentToNextClient(LOCALHOST, 8881, msg)
-        print("Client at ", self.clientAddress, " disconnected...")
-
-
-
-
+# class ClientThread(threading.Thread):
+# 
+#     def __init__(self, clientAddress, clientsocket):
+#         threading.Thread.__init__(self)
+#         self.csocket = clientsocket
+#         self.clientAddress = clientAddress
+#         print("New connection added: ", clientAddress)
+# 
+#     # def createMessageRoute(self, messageContent):
+#     #     msg = messageContent + SPACER + LOCALHOST +FIRST_PORT + SPACER + LOCALHOST + SECOND_PORT
+#     #     return  msg
+# 
+#     def run(self):
+#         print("Connection from : ", self.clientAddress)
+#         msg = ''
+#         while True:
+#             data = self.csocket.recv(2048)
+#             msg = data.decode()
+#             if msg=='bye':
+#                break
+#             print ("from client", msg)
+#             self.csocket.send(bytes("main server received message "+ msg, 'UTF-8'))
+#         #'hi::::127.0.0.1::::8881::::127.0.0.1::::8882'
+#         #msg = self.createMessageRoute(messageContent=msg)
+#             msg += SPACER + LOCALHOST + SPACER + FIRST_PORT + SPACER + LOCALHOST+ SPACER + SECOND_PORT
+#             print("creates message + route = " + msg )
+#             sentToNextClient(LOCALHOST, 8881, msg)
+#         print("Client at ", self.clientAddress, " disconnected...")
 
 
 def sentToNextClient(ip, port, msg):
