7c383a85e23563b4c08ff734d8a88b9912d9b4a8
maya_tavor <maya.tavor.04@gmail.com>
2021-11-24 19:02:55 +0200

    added a start message from the secondary servers to the main server

--- seconderyServers/server8883.py
+++ seconderyServers/server8883.py
@@ -3,8 +3,13 @@ import threading
 
 HOST = '127.0.0.1'  # Standard loopback interface address
 PORT = 8883         # Port to listen on
+MY_ID = 3
+
+MAIN_SERVER_PORT = 5678
 
 def sentToNextClient(ip, port, msg):
+    print("in sendtTONextClient, port = ", port, " ip = ", ip)
+    print("message = ", msg)
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((ip, int(port)))
 
@@ -27,17 +32,30 @@ def thread(conn):
         conn.send(list[0].encode())
     conn.close()
 
+def connectToMainServer(s):
+    msg = "100" + len(MY_ID) + MY_ID
+    s.send(msg.encode())
+
 
 def main():
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv.bind((HOST, PORT))
     serv.listen()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "127.0.0.1" #loopback
+    s.connect((host, MAIN_SERVER_PORT))
+
+    connectToMainServer(s)
+
     while True:
         conn, addr = serv.accept()
         x = threading.Thread(target=thread, args=(conn,))
         x.start()
 
     print('client disconnected')
+    serv.close()
+    s.close()
 
 
 if __name__ == "__main__":
