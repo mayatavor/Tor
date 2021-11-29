dcf5d41af70785f4ec6c2bf0e34826b6209352f8
Shelly Shuster <shelly.shuster2004@gmail.com>
2021-11-29 21:23:58 +0200

    Add createDBStructure fucntion to DatabaseAccess

--- MainServer/MainServer/MainServer/DatabaseAccess.cpp
+++ MainServer/MainServer/MainServer/DatabaseAccess.cpp
@@ -32,5 +32,21 @@ void DatabaseAccess::clear()
 
 bool DatabaseAccess::createDBstructure()
 {
-	return false;
+	char statement1[] = "CREATE TABLE IF NOT EXISTS Users(userId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, ipAddress TEXT NOT NULL); ";
+	createTable(statement1);
+	char statement2[] = "CREATE TABLE IF NOT EXISTS SecondaryServers(serverID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ipAddress TEXT NOT NULL, password TEXT NOT NULL, d INTEGER NOT NULL, e INTEGER NOT NULL); ";
+	createTable(statement2);
+	char statement3[] = "CREATE TABLE IF NOT EXISTS Chats(chatId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstUserId INTEGER NOT NULL, secondUSerId INTEGER NOT NULL); ";
+	createTable(statement3);
+	char statement4[] = "CREATE TABLE IF NOT EXISTS Favorites(userId INTEGER NOT NULL, chatId INTEGER NOT NULL); ";
+	createTable(statement4);
+}
+
+bool DatabaseAccess::createTable(char* sqlStatement)
+{
+	char** errMessage = nullptr;
+	int res = sqlite3_exec(_db, sqlStatement, nullptr, nullptr, errMessage);
+	if (res != SQLITE_OK)
+		return false;
+	return true;
 }
