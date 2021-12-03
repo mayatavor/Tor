5837b0414d7794d2b215b02ed379dda922610f4f
maya_tavor <maya.tavor.04@gmail.com>
2021-12-03 16:02:59 +0200

    changed the search option

--- gui/gui/MainWindow.xaml.cs
+++ gui/gui/MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace gui
             InitializeComponent();
             //this._communicator = c;
             this._communicator = new Communicator();
+
+            //get users from the server
             UserInfo user = new UserInfo(true, "maya");
             this.UsersList.Items.Add(user);
             UserInfo user2 = new UserInfo(false, "yoni");
@@ -74,20 +76,59 @@ namespace gui
             
         }
 
-        private void search_Click(object sender, RoutedEventArgs e)
-        {
-            string name = this.SearchText.Text;
-            this.SearchText.Text = "";
 
+        private void searchInUsers(string name)
+        {
             for (int i = 0; i < this.UsersList.Items.Count; i++)
             {
                 UserInfo user = (UserInfo)this.UsersList.Items[i];
 
-                if (!user.GetUsername().Contains(name))
+                if (!user.GetUsername().ToUpper().Contains(name.ToUpper()))
                 {
                     this.UsersList.Items.RemoveAt(i);
+                    i--;
                 }
             }
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < this.UsersList.Items.Count;)
+            {
+                this.UsersList.Items.RemoveAt(i);
+            }
+
+            UserInfo user = new UserInfo(true, "maya");
+            this.UsersList.Items.Add(user);
+            UserInfo user2 = new UserInfo(false, "yoni");
+            this.UsersList.Items.Add(user2);
+
+            this.SearchBackground.Source = GetImage("Assets/copy.png");
+            this.SearchText.Text = "";
+
+        }
+
+        private void SearchInText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+            {
+                string name = this.SearchText.Text;
+                name += e.Key.ToString();
+                searchInUsers(name);
+
+                this.SearchBackground.Source = GetImage("Assets/xSign.png");
+            }
+        }
+
+        private static BitmapImage GetImage(string imageUri)
+        {
+            var bitmapImage = new BitmapImage();
+
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(imageUri, UriKind.RelativeOrAbsolute);
+            bitmapImage.EndInit();
+
+            return bitmapImage;
         }
     }
 }
