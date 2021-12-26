e95f5aebb223dd9bc4701448f6b0ef02e4f037ba
SHUSTER SHELLY <shelly.shuster20041@gmail.com>
2021-12-26 16:33:40 +0200

    Added serialization

--- MainServer/MainServer/MainServer/serialize.cpp
+++ MainServer/MainServer/MainServer/serialize.cpp
@@ -1,7 +1,22 @@
 #include "serialize.h"
 #include "Structs.h"
+#include <iostream>
 
 std::string serialize::serializeUsers(std::list<std::string> allUsers, std::list<std::string> favorites)
 {
     std::string str = "";
+    std::list<std::string>::iterator it;
+    for (it = allUsers.begin(); it != allUsers.end(); it++) {
+        if (std::find(favorites.begin(), favorites.end(), *it) != favorites.end()) {
+            bool isGhost = (*it).find("ghost") != std::string::npos;
+            UsersListItem uli;
+            uli.isFavorite = true;
+            uli.isGhost = isGhost;
+            uli.usernameOther = *it;
+        }
+    }
+
+    //for (int i = 0; i < allUsers.size(); i++) {
+    //    if(std::find(favorites.begin(), favorites.end(), allUsers[i]) != )
+    //}
 }
