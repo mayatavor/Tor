e53dd07101317afaa57eaafe472b75e1a0ad150d
maya_tavor <maya.tavor.04@gmail.com>
2022-03-09 16:57:55 +0200

    secondery servers send port

--- seconderyServers/server8885.py
+++ seconderyServers/server8885.py
@@ -61,7 +61,7 @@ def thread(conn):
 connects to the main server and sends the public key with the server id
 """
 def connectToMainServer(s):
-    msg = "100" + "~" + str(MY_ID) + "~" + str(public_key[0]) + "~" + str(public_key[1])
+    msg = "100" + "~" + str(MY_ID) + "~" + str(public_key[0]) + "~" + str(public_key[1]) + "~" + MY_PORT
     msg = str(len(msg)).zfill(5) + msg
     s.send(msg.encode())
 
